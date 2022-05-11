967863f6ed71856ab3dfe84f1f4d62decd625984
Exe <exeeroot@gmail.com>
2022-05-11 15:26:09 -0400

    Agregado método de encriptación, queda verificar

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'Secret'
 app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://g25proyecto:g25proyecto1061@146.83.198.35:5432/g25proyecto_bd'
+app.config['SECURITY_PASSWORD_HASH'] = 'scrtcript'
+app.config['SECURITY_PASSWORD_SALT'] = 'fhasdgihwntlgy8f'
 
 from app import models, views
 
--- app/models.py
+++ app/models.py
@@ -1,4 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
 import json
+from flask_security import Security, SQLAlchemyUserDatastore, UserMixin
+import bcrypt
 
 db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password not readable')
+    @password.setter
+    def password(self, password):
+        self.password_hash = bcrypt.hashpw(password, bcrypt.gensalt())
+        
+    def verify_password(self, password):
+        return bcrypt.hashpw(password,bcrypt.gensalt()) == self.password_hash
