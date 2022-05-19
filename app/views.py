32045a1453cf47e830bb4aa5b6adf24bb26ed53f
Exe <exeeroot@gmail.com>
2022-05-19 18:01:52 -0400

    Funciones de encriptación

--- app/views.py
+++ app/views.py
@@ -5,9 +5,8 @@ db = models.db
 
 @app.route('/')
 def index():
-    
+
     new_user = models.User(id = 1, email = 'aaaa@gmail.com', password = b'ewe')
     print(new_user.verify_password(b'ewe'))
-    print(new_user.password)
     
     return render_template("home.html")
