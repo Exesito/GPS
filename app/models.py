2cc89ef0cb2ad6bd930664bab5bd0169d68faf8e
matrixnacho <ignaciourrutia021@gmail.com>
2022-05-19 21:09:03 -0400

    Update models.py
    added db model for horario

--- app/models.py
+++ app/models.py
@@ -1,4 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
 import json
 
+from sqlalchemy import true
+
 db = SQLAlchemy()
+
+class domo_horario(db.Model):
+    hor_id = db.Column(db.Integer, primary_key = true)
+    rtr_id = db.Column(db.Integer)
+    hor_diainicio = db.Column(db.Integer)
+    hor_diatermino = db.Column(db.Integer)
+    hor_horainicio = db.Column(db.Time)
+    hor_horatermino = db.Column(db.Time)
+    hor_disponibilidad = db.Column(db.Bool)
+
+class domo_rtr(db.Model):
+    rtr_id = db.Column(db.Integer, primary_key = true)
+    dir_id = db.Column(db.Integer)
+    tpr_id = db.Column(db.Integer)
+    rtr_nombre = db.Column(db.String(50))
+    rtr_rutacarta = db.Column(db.Text)
+    
