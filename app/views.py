0eb124dc1cdc9ee1ad1defc78c48459f9d1f90f5
matrixnacho <ignaciourrutia021@gmail.com>
2022-05-19 23:07:02 -0400

    se añade un view para el editor de horario además del template html

--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
 from app import app, models
 from flask import render_template
 
-db = models.db
-
 @app.route('/')
 def index():
 
     return render_template("home.html")
+
+@app.route('/editor_horario')
+def editor_horario():
+    return(render_template('templates/editor_horario'))
