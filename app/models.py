9a46b51348d9c07e4ba9c77bfed16369d7a461e7
matrixnacho <ignaciourrutia021@gmail.com>
2022-05-19 21:55:18 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 
 class domo_horario(db.Model):
     hor_id = db.Column(db.Integer, primary_key = true)
-    rtr_id = db.Column(db.Integer)
+    rtr_id = db.Column(db.Integer ,db.ForeignKey('domo_rtr.rtr_id'))
     hor_diainicio = db.Column(db.Integer)
     hor_diatermino = db.Column(db.Integer)
     hor_horainicio = db.Column(db.Time)
@@ -16,8 +16,28 @@ class domo_horario(db.Model):
 
 class domo_rtr(db.Model):
     rtr_id = db.Column(db.Integer, primary_key = true)
-    dir_id = db.Column(db.Integer)
+    dir_id = db.Column(db.Integer, db.ForeignKey('domo_direccion.dir_id'))
     tpr_id = db.Column(db.Integer)
     rtr_nombre = db.Column(db.String(50))
     rtr_rutacarta = db.Column(db.Text)
+    rtr_descripcion = db.Column(db.Text)
+    rtr_opvega = db.Column(db.Boolean)
+    rtr_opvege = db.Column(db.Boolean)
+    rtr_duenonombre = db.Column(db.String(40))
+    rtr_duenoapellido = db.Column(db.String(40))
+
+class domo_direccion(db.Model):
+    dir_id = db.Column(db.Integer, primary_key = true)
+    ciu_id = db.Column(db.Integer, db.ForeignKey('domo_ciudad.ciu_id'))
+    dir_nombrecalle = db.Column(db.String(50))
+    dir_numerocalle = db.Column(db.Integer)
+
+class domo_ciudad(db.Model):
+    ciu_id = db.Column(db.Integer, primary_key = true)
+    reg_id = db.Column(db.Integer, db.ForeignKey('domo_region.reg_id'))
+    ciu_nombre = db.Column(db.String(40))
+
+class domo_region(db.Model):
+    reg_id = db.Column(db.Integer, primary_key = true)
+    reg_nombre = db.Column(db.String(30))
 
