9f5154e6e77d3855d968e91a4dfdb5f247d39c59
matrixnacho <ignaciourrutia021@gmail.com>
2022-08-04 23:03:39 -0400

    updates cu nacho
    se arreglan links en navbar, se añade una tabla en editor_horario.html para ostrar los horarios y se añade URI a bdd de testing en init.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'Secret'
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://g25proyecto:g25proyecto1061@146.83.198.35:5432/g25proyecto_bd'
+#app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://g25proyecto:g25proyecto1061@146.83.198.35:5432/g25proyecto_bd'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:peterfields@190.47.90.186:5432/domo_testing'
+
 
 from app import models, views
 
