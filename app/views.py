2e3bddb6919175007c186dc9482358c9d814e989
Exe <exeeroot@gmail.com>
2022-08-20 13:53:18 -0400

    Realización de reserva segun medio de pago

--- app/views.py
+++ app/views.py
@@ -2,8 +2,8 @@
 from multiprocessing.connection import Client
 from app import app, models
 from app.forms import RegisterForm, ReservaForm, MesaForm, ClientForm
-from app.models import User, domo_restaurante
-from flask import render_template, request, url_for, redirect
+from app.models import User, domo_cliente, domo_reserva, domo_restaurante
+from flask import render_template, request, url_for, redirect, session
 
 db = models.db
 
@@ -35,14 +35,15 @@ def register():
 @app.route('/reservar/<id>', methods=['GET','POST'])
 def reservar(id):
     
-    registered = 1
+    #session["cli_id"] = 1
     
     day_choice = [1,2,3]
     hour_choice = [1,2,3]
     
     form = ReservaForm()
     mesa_form = MesaForm()
-
+    client_form = ClientForm()
+    
     form.dia.choices = day_choice
     form.hora.choices = hour_choice
     
@@ -53,14 +54,13 @@ def reservar(id):
         mesa_form.mesa.choices.append((item.msa_id, item.msa_numero))
     
     data = {
-        #"RESTAURANTE_ID": models.domo_restaurante.get_by_id(id),
         "RESTAURANTE_ID": restaurante.rtr_id,
         "nombre": restaurante.rtr_nombre,
         "descripcion": restaurante.rtr_descripcion
     }
     
     
-    return render_template("cli_reservar.html", data = data, registered = registered, form = form, mesa_form = mesa_form)
+    return render_template("cli_reservar.html", data = data, form = form, mesa_form = mesa_form, client_form = client_form)
 
 @app.route('/reservar/<id_restaurante>/<id_reserva>/datos_cliente', methods=['GET','POST'])
 def reserva_not_registered(id_restaurante, id_reserva):
@@ -70,7 +70,6 @@ def reserva_not_registered(id_restaurante, id_reserva):
     restaurante = models.domo_restaurante.get_by_id(id_restaurante)
     
     data = {
-        #"RESTAURANTE_ID": models.domo_restaurante.get_by_id(id),
         "RESTAURANTE_ID": restaurante.rtr_id,
         "nombre": restaurante.rtr_nombre,
         "descripcion": restaurante.rtr_descripcion,
@@ -92,9 +91,37 @@ def reserva_create(id_restaurante):
         restaurante = domo_restaurante.get_by_id(id_restaurante)
         id_reserva = restaurante.generate_reserva(hora, fecha, mesa_id, estado)
         
+        if session.get('cli_id') is None:
+            return redirect(url_for('reserva_not_registered', id_restaurante = id_restaurante, id_reserva = id_reserva))
+        
+        reserva = domo_reserva.get_by_id(id_reserva)
+        
+        medio_de_pago = request.form["medio_de_pago"]
+        
+        dict_mdp = {
+            "DEBITO": 1,
+            "EFECTIVO": 2,
+            "WEBPAY": 3
+        }
+        
+        reserva.cli_id = session["cli_id"]
+        reserva.tpg_id = dict_mdp[medio_de_pago] 
+        
+        if medio_de_pago is "WEBPAY":
+            pass
+        
+        reserva.estado = "REALIZADA"
+        db.session.commit()
         
         return redirect(url_for('reserva_exitosa', id_restaurante = id_restaurante, id_reserva = id_reserva))
 
+
+@app.route('/reservar/<id_restaurante>/<id_reserva>/update_reserva', methods=['GET','POST'])
+def reserva_update_new_client(id_restaurante, id_reserva):
+    
+    if request.method == 'POST':
+        
+        
 
 @app.route('/reservar/<id_restaurante>/<id_reserva>/reserva_exitosa', methods=['GET','POST'])
 def reserva_exitosa(id_restaurante, id_reserva):
@@ -116,3 +143,9 @@ def res_ver_reservas():
     return render_template("res_ver_reservas.html")
 
 ########- FIN DE CASO DE USO -#################
+
+@app.route('/logout', methods=['GET','POST'])
+def logout():
+    
+    session.clear()
+    return redirect(url_for('index'))
