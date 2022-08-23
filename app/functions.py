9c518b6a3d718ab2efee36c98d562db5badb2e09
GonzaloNC <93685321+GonzaloNC@users.noreply.github.com>
2022-08-23 19:55:37 -0400

    Cambio a funciones para la funcion de emails
    Función para enviar emails, usa el correo del destinatario, mensaje y asunto

--- app/functions.py
+++ app/functions.py
@@ -1,2 +1,18 @@
-from app import app, models, views
+#from app import app, models, views
+import smtplib
+from email.mime.text import MIMEText
 
+def enviar_email(destinatario, mensaje, asunto):
+
+    email_domo = "teamdomorestaurantes@gmail.com"
+    passw = "yuyfufiubxaeclpz"
+    message = MIMEText(mensaje)
+    message['Subject'] = asunto
+    message['From'] = "teamdomorestaurantes@gmail.com"
+    message['To'] = destinatario
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email_domo, passw)
+    server.sendmail(email_domo, destinatario, message.as_string())
+    server.quit()
